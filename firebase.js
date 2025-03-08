544447010ff569cf0c87693b5277e455c370f9fc
Bennett Schoonerman <22403266+BennettTheTiger@users.noreply.github.com>
2025-03-08 14:27:04 -0500

    Sprint2 - Firebase Config (#17)
    * sprint 2 server updates to support the UI
    
    * Sprint 2 setup error classes and standard error handling
    
    * update other routes to use error handling middleware
    
    * robust error handling and switch to firebase Auth
    
    * Sprint 2 testing updates
    
    * lint
    
    * auth and error handling updates
    
    * sprint 2 more firebase updates
    
    * Sprint 2 return liked names as array
    
    * sprint 2 - edit and view user favorites
    
    * Sprint 2 - new nextNames route with nameContext filtering
    
    * Sprint 2 - filter on virtual gender fields
    
    * sprint 2 test coverage
    
    * Sprint 2 update name model to reflect data
    
    * Sprint 2 boost unit test coverage
    
    * Sprint 2 fix firebase settings

--- firebase.js
+++ firebase.js
@@ -1,7 +1,19 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "nameflame",
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": "nameflameserver@nameflame.iam.gserviceaccount.com",
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/nameflameserver%40nameflame.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
