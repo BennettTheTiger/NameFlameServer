3780b424c4f38aad60fba14dff26be9838783e10
Bennett Schoonerman <bennett411@gmail.com>
2025-04-13 09:19:02 -0400

    sprint 4 new route for updating notification preferences

--- routes/v1/auth/systemUser.js
+++ routes/v1/auth/systemUser.js
@@ -6,11 +6,31 @@ const _ = require('lodash');
 
 const router = express.Router();
 
+function trimResponse(user) {
+  return _.pick(user, ['id', 'email', 'theme', 'allowNotifications']);
+}
+
 router.get('/systemUser', authMiddleware, async (req, res, next) => {
   try {
     const systemUser = await User.findOne({ firebaseUid: req.userData.uid });
     logger.info(`System user ${systemUser.id} found`);
-    const userData = _.pick(systemUser, ['id', 'email', 'theme', 'allowNotifications']);
+    const userData = trimResponse(systemUser);
+    res.status(200).send(userData);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch('/systemUser', authMiddleware, async (req, res, next) => {
+  try {
+    const dataToUpdate = _.pick(req.body, ['theme', 'allowNotifications']);
+    const systemUser = await User.findOneAndUpdate(
+      { firebaseUid: req.userData.uid },
+      dataToUpdate,
+      { new: true }
+    );
+    logger.info(`System user ${systemUser.id} updated`);
+    const userData = trimResponse(systemUser);
     res.status(200).send(userData);
   } catch (err) {
     next(err);
